51bdc20a9447b6e1d492bc5e07611802666ed153
Pongo <echotech8310@gmail.com>
2025-09-13 16:57:53 +0700

    update link logo

--- src/app/components/brand/page.tsx
+++ src/app/components/brand/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Brand({ isVisible }: { isVisible?: boolean }) {
     return (
-        <div className="text-lg font-bold flex items-center gap-2">
-            <Link href="/">
+        <div className="text-lg font-bold">
+            <Link href="/" className="flex items-center gap-2">
                 <Image src="/logo.png" alt="logo" width={100} height={80} className="h-12 w-fit" />
             </Link>
         </div>
