4a47ea3357b734d4633123d2c0218d578c21dab4
Pongo <echotech8310@gmail.com>
2025-09-13 17:02:04 +0700

    update logo

--- src/app/components/brand/page.tsx
+++ src/app/components/brand/page.tsx
@@ -5,6 +5,7 @@ export default function Brand({ isVisible }: { isVisible?: boolean }) {
         <div className="text-lg font-bold">
             <Link href="/" className="flex items-center gap-2">
                 <Image src="/logo.png" alt="logo" width={100} height={80} className="h-12 w-fit" />
+                <Image src="/Optimal.png" alt="brand" width={100} height={80} className="h-10 w-fit" />
             </Link>
         </div>
     );
