117b1458dd8e5d95505ac335c772e23a332de53d
Pongo <echotech8310@gmail.com>
2025-09-14 16:16:52 +0700

    refactor FAQs component to improve responsive design for contact section

--- src/app/components/faqs/page.tsx
+++ src/app/components/faqs/page.tsx
@@ -33,8 +33,8 @@ export default function Faqs() {
                     </div>
                 </div>
 
-                <div className="text-center mt-12 px-12 py-8 bg-purple-800 rounded-lg w-1/2 mx-auto text-white">
-                    <h1 className="text-2xl font-semibold mb-6">Masih Punya pertanyaan terkait UKOM?</h1>
+                <div className="text-center mt-12 px-12 py-8 bg-purple-800 rounded-lg max-md:w-full w-1/2  mx-auto text-white">
+                    <h1 className="max-sm:text-base text-2xl font-semibold mb-6">Masih Punya pertanyaan terkait UKOM?</h1>
                     <a href="/faq" className="flex items-center w-fit mx-auto bg-white text-green-700 font-semibold px-6 py-3 rounded-lg hover:bg-transparent hover:text-white border-2 border-transparent hover:border-white transition-colors">
                         <Icon icon="tabler:brand-whatsapp" className="mr-2 size-6" width="32" height="32" />
                         Hubungi Kami
