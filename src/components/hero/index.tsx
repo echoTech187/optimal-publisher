5d73343b55f38e7d24f7a2a11a57bf7bc839b509
echotech187 <echotech187@gmail.com>
2025-10-30 17:21:09 +0700

    refactor: improve layout and responsiveness of hero section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 export default function Header() {
     return (
         <>
-            <section className="relative w-full h-auto lg:h-[800px] flex items-center justify-center py-20 lg:py-0" id="hero">
+            <section className="relative max-md:h-screen w-full h-auto lg:h-full flex items-center justify-center pt-20 md:py-20 lg:py-0" id="hero">
                 <div className="absolute top-0 left-0 bg-gradient-to-r from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 min-h-[100%] w-full -z-0"></div>
-                <div className="flex gap-4 container mx-auto px-4 2xl:px-0 w-full z-10">
+                <div className="flex gap-4 container mx-auto px-4 2xl:px-0 w-full z-10 h-full justify-center items-center">
                     <div className="w-full lg:w-1/2 flex flex-col gap-4 text-center lg:text-left items-center lg:items-start">
                         <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold leading-tight flex flex-col">
                             <p>Solusi Digital</p>
@@ -19,7 +19,7 @@ export default function Header() {
                             <br />Mau tahu tips dan trik jawab soal dengan cepat?
                             <br /><b>Optimal Solusinya!</b> Sebuah platform belajar tes CPNS kapan saja dan dimana aja!
                         </p>
-                        <div className="flex flex-col md:flex-row flex-wrap gap-4">
+                        <div className="flex items-center justify-center flex-wrap gap-4">
                             <span className="inline-block py-1 text-sm rounded-full before:content-['✓'] before:p-1 before:w-5 before:h-5 before:rounded-full before:text-center before:inline-block before:bg-orange-300 before:font-fontawesome before:mr-2 before:text-xs">Mulai Gratis</span>
                             <span className="inline-block py-1 text-sm rounded-full before:content-['✓'] before:p-1 before:w-5 before:h-5 before:rounded-full before:text-center before:inline-block before:bg-orange-300 before:font-fontawesome before:mr-2 before:text-xs">Banyak Pilihan Paket</span>
                             <span className="inline-block py-1 text-sm rounded-full before:content-['✓'] before:p-1 before:w-5 before:h-5 before:rounded-full before:text-center before:inline-block before:bg-orange-300 before:font-fontawesome before:mr-2 before:text-xs">Langsung Lihat Skor</span>
